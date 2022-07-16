0d8ae541dbb4d1d75dfadb00f2fa758d0f96bd5f
smrenato <renatosmx@gmail.com>
2022-07-16 17:28:13 -0300

    feat: Template

---
+++ components/component.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "component.h"
+
+void someFunction(void)
+{
+    printf("New component added\n");
+}
---
+++ main/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "component.h"
+
+
+void app_main(void)
+{
+    printf("Hello world!\n");
+
+    someFunction();
+}
